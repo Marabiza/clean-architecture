b56900250ab31b2f535732c3a2e7d26de6510b46
M519333 <vitor.carretero@bradescoseguros.com.br>
2025-07-16 13:32:34 -0300

    Criado os notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,22 +1,23 @@
 import Product from "./product";
+import NotificationError from "../../@shared/notification/notification.error";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
   });
 
   it("should change name", () => {
@@ -29,5 +30,19 @@ describe("Product unit tests", () => {
     const product = new Product("123", "Product 1", 100);
     product.changePrice(150);
     expect(product.price).toBe(150);
+  });
+
+  it("should accumulate two errors at the same time", () => {
+    try {
+      new Product("", "", 100);
+    } catch (error) {
+      const notificationError = error as NotificationError;
+      expect(notificationError).toBeInstanceOf(NotificationError);
+      expect(notificationError.errors).toEqual([
+        { context: "product", message: "Id is required" },
+        { context: "product", message: "Name is required" },
+      ]);
+      expect(notificationError.message).toBe("product: Id is required,product: Name is required");
+    }
   });
 });
